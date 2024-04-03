8fe813e517a272b0594e2a310e58ec4bf5a592b0
gauravk2024 <gaurav.khairnar@thinkitive.com>
2024-04-03 12:30:04 +0530

    dayTwoJS

--- Synchronous Vs. Asynchronous Code/Synchronous Vs. Asynchronous Code.js
+++ Synchronous Vs. Asynchronous Code/Synchronous Vs. Asynchronous Code.js
@@ -13,3 +13,12 @@ setTimeout(()=>{
 }, 4000)
 console.log("Good Evening")
 
+let p = new Promise((resolve, reject)=>{
+    setInterval(()=>{
+        resolve(67);
+    }, 2000)
+}).then(()=>{
+    console.log("Success")
+}).catch(()=>{
+    console.log("failed")
+})
